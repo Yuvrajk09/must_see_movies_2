500da77f29e4417b32ce2f8db8ed607d681022a1
anonymous anonymous <yuvraj.karan@kellogg.northwestern.edu>
2021-11-16 04:04:25 +0000

    Whitelist columns in ReviewDetail for ActiveAdmin

--- app/admin/review_details.rb
+++ app/admin/review_details.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ReviewDetail do
 
+ permit_params :review_id, :review, :star_ratings
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
