e2c9735d7017d6b2c9dfe7f353b75ff9e07344da
anonymous anonymous <yuvraj.karan@kellogg.northwestern.edu>
2021-11-16 04:04:28 +0000

    Whitelist columns in BookmarkStatus for ActiveAdmin

--- app/admin/bookmark_statuses.rb
+++ app/admin/bookmark_statuses.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BookmarkStatus do
 
+ permit_params :bookmark_id, :bookmark_status
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
